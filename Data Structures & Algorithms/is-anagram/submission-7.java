a89d74b882b853b683ad1c42a89281600086e79f
Malhar Rathod <mrathod34@gatech.edu>
2026-08-19 23:10:15 -0400

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        HashMap<Character, Integer> stringS = new HashMap<>();
+        HashMap<Character, Integer> stringT = new HashMap<>();
+            for (char c : s.toCharArray()) {
+                stringS.put(c, stringS.getOrDefault(c, 0) + 1);
+            } 
+            for (char c : t.toCharArray()) {
+                stringT.put(c, stringT.getOrDefault(c, 0) + 1);
+            }       
+            return stringS.equals(stringT);
+    }
+}
+
