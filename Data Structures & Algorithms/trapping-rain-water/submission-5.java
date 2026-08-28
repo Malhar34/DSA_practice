645fb632c8b5f3e8d57c4140c9abf3c0d425d77b
Malhar Rathod <mrathod34@gatech.edu>
2026-08-28 02:20:02 -0400

    Add: trapping-rain-water - submission-5

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int total = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                leftMax = Math.max(leftMax, height[left]);
+                total += leftMax - height[left];
+                left++;
+            } else if (height[left] > height[right] || height[left] == height[right] ) {
+                rightMax = Math.max(rightMax, height[right]);
+                total += rightMax - height[right];
+                right--;
+            } 
+        }
+        return total;
+    }
+}
